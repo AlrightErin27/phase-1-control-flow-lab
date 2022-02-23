faa1bd5c4d11dc16f6c31d3aa35b4fea0502688f
Erin Van Brunt <erinmontybruce@gmail.com>
2022-02-23 09:20:47 -0800

    done

--- index.js
+++ index.js
@@ -2,14 +2,42 @@ function scuberGreetingForFeet(feet) {
   if (feet <= 400) {
     return "This one is on me!";
   } else if (feet > 2000) {
-    return "I will gladly take your thirty bucks.";
+    if (feet > 2500) {
+      return "No can do.";
+    } else {
+      return "I will gladly take your thirty bucks.";
+    }
   }
 }
 
-function ternaryCheckCity() {
-  // Write your code here!
-}
+// function ternaryCheckCity(city) {
+//   if (city === "NYC") {
+//     return "Ok, sounds good.";
+//   } else {
+//     return "No go.";
+//   }
+// }
 
-function switchOnCharmFromTip() {
-  // Write your code here!
+const ternaryCheckCity = (city) =>
+  city === "NYC" ? "Ok, sounds good." : "No go.";
+
+// function switchOnCharmFromTip(str) {
+//   if ((str = "generous")) {
+//     return "Thank you so much.";
+//   } else if ((str = "not as generous")) {
+//     return "Thank you.";
+//   } else {
+//     return "Bye.";
+//   }
+// }
+
+function switchOnCharmFromTip(str) {
+  switch (str) {
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    default:
+      return "Bye.";
+  }
 }

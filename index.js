918fc75a08b630217b3f7de7f2f0ec701164f8dc
Erin Van Brunt <erinmontybruce@gmail.com>
2022-02-23 08:49:23 -0800

    initial commit

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet) {
+  if (feet <= 400) {
+    return "This one is on me!";
+  } else if (feet > 2000) {
+    return "I will gladly take your thirty bucks.";
+  }
+}
+
+function ternaryCheckCity() {
   // Write your code here!
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip() {
   // Write your code here!
 }
